71a636915f6b9a956c5fe8d5bcffc39a6f5f9426
Zeyu Li <lizeyu090312@gmail.com>
2023-10-24 23:00:35 -0400

    FP_layers

--- HW4/FP_layers.py
+++ HW4/FP_layers.py
@@ -14,39 +14,59 @@ class STE(torch.autograd.Function):
         '''
         if bit is None:
             wq = w
-        elif bit==0:
-            wq = w*0
+        elif bit == 0:
+            wq = w * 0
         else:
             # Build a mask to record position of zero weights
-            weight_mask = ...
+            np_weights = w.detach().cpu().numpy()
+            weight_mask = np.ma.masked_equal(x=np_weights, value=0)  # zero will be marked true
+            weight_mask = torch.tensor(1 - np.ma.getmask(weight_mask).astype(int)).to(device)  
+            # zero will be marked zero, weight_mask onto GPU
+            
+            flattened_np_weights = np_weights.flatten()
             
             # Lab3 (a), Your code here:
             if symmetric == False:
+                max_, min_ = np.max(flattened_np_weights), np.min(flattened_np_weights)
                 # Compute alpha (scale) for dynamic scaling
-                alpha = ...
+                alpha = max_ - min_
                 # Compute beta (bias) for dynamic scaling
-                beta = ...
+                beta = min_
                 # Scale w with alpha and beta so that all elements in ws are between 0 and 1
-                ws = ...
+                ws = (w - beta) / alpha
                 
-                step = 2 ** (bit)-1
+                step = (2 ** (bit)) - 1
                 # Quantize ws with a linear quantizer to "bit" bits
-                R = ...
+                R = (1/step) * torch.round(step * ws)  # r_o = 1/(2^k-1) * round((2^k-1)*r_i), assume r_i=r_in=ws
                 # Scale the quantized weight R back with alpha and beta
-                wq = ...
+                wq = alpha * R + beta
             
             # Lab3 (e), Your code here:
-            else:
+            else:  # if symmetric == True
+                # In symmetric quantization, the quantization levels are symmetric to zero.
+                abs_max_ = np.max(np.abs(flattened_np_weights))
+                alpha = abs_max_ * 2
+                beta = 0  # do not shift
+                
+                # Scale w with alpha and beta=0 so that all elements in ws are between -0.5 and 0.5
+                ws = ((w - beta) / alpha)
+                
+                step = (2 ** (bit)) - 1
+                
+                # Quantize ws with a linear quantizer to "bit" bits
+                R = (1/step) * torch.round(step * ws)
+                # Scale the quantized weight R back with alpha and beta
+                wq = alpha * R + beta
                 pass
 
             # Restore zero elements in wq 
-            wq = wq*weight_mask
+            wq = wq * weight_mask
             
         return wq
 
     @staticmethod
     def backward(ctx, g):
-        return g, None, None  # my addition to fix the "not enough outputs" bug
+        return g, None, None  # my addition. Originally return g, None
 
 class FP_Linear(nn.Module):
     def __init__(self, in_features, out_features, Nbits=None, symmetric=False):
