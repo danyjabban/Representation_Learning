c8ca3814fda85acb0cef015c270d3105129147c9
Michael Li <zl310@duke.edu>
2023-11-27 23:33:32 -0500

    runnable

--- project_test/train_class.py
+++ project_test/train_class.py
@@ -51,64 +51,32 @@ class TrainerDDP():
         self.optimizer = LARS(self.model.parameters(), lr=self.lr, momentum=0.9, weight_decay=self.l2_reg, nesterov=False)
         self.warmup_iters = 10
         self.scheduler_warmup = optim.lr_scheduler.LinearLR(self.optimizer, start_factor=0.01, end_factor=1.0, 
-                                                            total_iters=self.warmup_iters, verbose=True)
-        self.scheduler_after  = optim.lr_scheduler.CosineAnnealingLR(self.optimizer, T_max=1000, verbose=True)
-        cifar_dataloader_ddp(self.batch_size)
-        return
-    
-    def cifar_dataloader_ddp(self, bs: int):
-        self.sampler_train = DistributedSampler(trainset)
-        # transform_train = transforms.Compose([
-        #     # transforms.RandomCrop(32, padding=4),
-        #     # transforms.RandomHorizontalFlip(),
-        #     transforms.ToTensor(),  # transforms happen for each epoch
-        #     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        # ])
-
-        # transform_test = transforms.Compose([
-        #     transforms.ToTensor(),
-        #     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        # ])
-        transform_gen = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ])
-        trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform_gen)
-        self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=False, sampler=sampler_train, 
-                                                num_workers=16)
-
-        testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_gen)
-        self.testloader = torch.utils.data.DataLoader(testset, batch_size=bs, shuffle=False, 
-                                                sampler=DistributedSampler(testset, shuffle=False), num_workers=8)
-        return trainloader, testloader, sampler_train
-
-    def _save_checkpoint(self, epoch: int):
-        ckp = self.model.state_dict()
-        model_path = self.const["trained_models"] / f"CIFAR10_ddp_epoch{epoch}.pt"
-        torch.save(ckp, model_path)
+                                                            total_iters=self.warmup_iters, verbose=False)
+        self.scheduler_after  = optim.lr_scheduler.CosineAnnealingLR(self.optimizer, T_max=1000, verbose=False)
+        self.trainloader, self.testloader, self.sampler_train = TrainerDDP.cifar_dataloader_ddp(self.batch_size)
         return
 
-    def _run_epoch(epoch):
+    def _run_epoch(self, epoch):
         self.trainloader.sampler.set_epoch(epoch)
         # T_0 = epochs -> no restart? Paper uses lin warmup (first 10 epochs) then cosine decay schedule w/o restarts
 
         global_steps = 0
-        start = time.time()
 
-        if epoch >= warmup_iters:
-            scheduler = scheduler_after
+        if epoch >= self.warmup_iters:
+            scheduler = self.scheduler_after
         else:
-            scheduler = scheduler_warmup
+            scheduler = self.scheduler_warmup
         """
         Start the training code.
         """
-        print('\nEpoch: %d' % epoch)
+        if self.gpu_id == 0:
+            print('\nEpoch: %d' % epoch)
         self.model.train()
         train_loss = 0
         total = 0
         for batch_idx, (inputs, targets) in enumerate(self.trainloader):
-            augment_inputs1 = TrainerDDP.augment_data_separate(inputs).to(device)
-            augment_inputs2 = TrainerDDP.augment_data_separate(inputs).to(device)
+            augment_inputs1 = TrainerDDP.aug_dat(inputs).to(self.gpu_id)
+            augment_inputs2 = TrainerDDP.aug_dat(inputs).to(self.gpu_id)
             del inputs
             self.optimizer.zero_grad()
             outputs1 = self.model(augment_inputs1)
@@ -126,12 +94,10 @@ class TrainerDDP():
             total += targets.size(0)
             global_steps += 1
 
-            if global_steps % log_every_n == 0:
-                end = time.time()
-                num_examples_per_second = log_every_n * batch_size / (end - start)
-                print("[Step=%d]\tLoss=%.4f\t%.1f examples/second" 
-                        % (global_steps, train_loss / (batch_idx + 1), num_examples_per_second))
-                start = time.time()
+            if global_steps % self.log_every_n == 0:
+                if self.gpu_id == 0:
+                    print("[Step=%d]\tLoss=%.4f" 
+                            % (global_steps, train_loss / (batch_idx + 1)))
 
             scheduler.step()
 
@@ -144,58 +110,38 @@ class TrainerDDP():
         total = 0
         with torch.no_grad():
             for batch_idx, (inputs, targets) in enumerate(self.testloader):
-                augment_inputs1, augment_inputs2 = TrainerDDP.augment_data_separate(inputs).to(device), TrainerDDP.augment_data_separate(inputs).to(device)
+                augment_inputs1, augment_inputs2 = TrainerDDP.aug_dat(inputs).to(self.gpu_id), TrainerDDP.aug_dat(inputs).to(self.gpu_id)
                 outputs1, outputs2 = self.model(augment_inputs1), self.model(augment_inputs2)
                 loss = self.criterion(outputs1, outputs2)
 
                 test_loss += loss.item()
                 total += targets.size(0)
-        num_val_steps = len(testloader)
+        num_val_steps = len(self.testloader)
         val_loss = test_loss / num_val_steps
-        print("Test Loss=%.4f" % val_loss)
-
-        if val_loss < best_loss:
-            best_loss = val_loss
+        if self.gpu_id == 0:
+            print("Test Loss=%.4f" % val_loss)
         return
 
-    def _save_checkpoint(epoch):
+    def _save_checkpoint(self, epoch: int, save_base_path: str):
         print("Saving...")
-        torch.save(self.model.state_dict(), "test_model_epoch_%d_bs_%d_lr_%g_reg_%g.pt" 
-                                        % int(epoch, self.batch_size, self.lr, self.l2_reg))
+        torch.save(self.model.state_dict(), "%s/epoch_%d_bs_%d_lr_%g_reg_%g.pt" 
+                                        % (save_base_path, int(epoch), int(self.batch_size), self.lr, self.l2_reg))
         return
 
-    def train(self, max_epochs: int):
+    def train(self, max_epochs: int, save_base_path: str):
         self.model.train()
         for epoch in range(max_epochs):
             # https://pytorch.org/docs/stable/data.html#torch.utils.data.distributed.DistributedSampler
             self.sampler_train.set_epoch(epoch)
             self._run_epoch(epoch)
             # only save once on master gpu
-            if self.gpu_id == 0 and (epoch+1) % 100 == 0:
-                self._save_checkpoint(epoch)
+            if self.gpu_id == 0 and ((epoch+1) % 100 == 0 or epoch == 0):
+                self._save_checkpoint(epoch, save_base_path)
         # save last epoch
-        self._save_checkpoint(max_epochs - 1)
+        self._save_checkpoint(max_epochs - 1, save_base_path)
 
-
-    # def test(self, final_model_path: str):
-    #     self.model.load_state_dict(
-    #         torch.load(final_model_path, map_location="cpu")
-    #     )
-    #     self.model.eval()
-
-    #     with torch.no_grad():
-    #         for src, tgt in self.testloader:
-    #             src = src.to(self.gpu_id)
-    #             tgt = tgt.to(self.gpu_id)
-    #             out = self.model(src)
-    #             self.valid_acc.update(out, tgt)
-    #     print(
-    #         f"[GPU{self.gpu_id}] Test Acc: {100 * self.valid_acc.compute().item():.4f}%"
-    #     )
-    #     return
-    
     @staticmethod
-    def augment_data_separate(batch):
+    def aug_dat(batch):
         """
         Apply transforms once on one batch without reserving space for the others
         """
@@ -207,19 +153,41 @@ class TrainerDDP():
                                     ])
         # augment(batch) should be size [batch_size*1, 1, 32, 32]
         return augment(batch)
+    
+    @staticmethod
+    def cifar_dataloader_ddp(bs: int):
+        transform_gen = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+        
+        trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform_gen)
 
-def run_main(rank, world_size, total_epochs, batch_size, lr, reg, head, log_every_n):
+        sampler_train = DistributedSampler(trainset)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=False, sampler=sampler_train, 
+                                                  num_workers=16, pin_memory=True)
+
+        testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_gen)
+        sampler_test = DistributedSampler(testset, shuffle=False)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=bs, shuffle=False, 
+                                                 sampler=sampler_test, num_workers=8, pin_memory=True)
+        return trainloader, testloader, sampler_train
+
+
+def run_main(rank, world_size, total_epochs, batch_size, lr, reg, head, save_base_path, log_every_n):
     ddp_setup(rank, world_size)
     model = ResNetCIFAR(head_g=head)
     trainer = TrainerDDP(rank, model, batch_size, lr, reg, log_every_n=log_every_n)
-    trainer.train(total_epochs)
+    trainer.train(total_epochs, save_base_path)
     destroy_process_group()
 
 if __name__ == "__main__":
     # from lars.lars import LARS
     head = nn.Sequential(FP_Linear(64, 64, Nbits=None), nn.ReLU(True), FP_Linear(64, 64, Nbits=None))
+    save_base_path = "./saved_models/"
+    os.makedirs(save_base_path, exist_ok=True)
     # model = nn.parallel.DistributedDataParallel(ResNetCIFAR(head_g=head))
     # model = nn.DataParallel(ResNetCIFAR(head_g=head))
     batch_size = int(1024)
     world_size = torch.cuda.device_count()
-    mp.spawn(run_main, args=(world_size, 1000, batch_size, 0.3*batch_size/256, 1e-6, head, 50,), nprocs=world_size)
+    mp.spawn(run_main, args=(world_size, 1000, batch_size, 0.3*batch_size/256, 1e-6, head, save_base_path, 50,), nprocs=world_size)
