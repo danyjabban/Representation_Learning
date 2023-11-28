7bee67fd9f4b7dd354ebe60fbf59eafc75f1c4f2
Michael Li <zl310@duke.edu>
2023-11-27 20:15:27 -0500

    messy

--- project_test/resnet.py
+++ project_test/resnet.py
@@ -6,6 +6,7 @@ from FP_layers import *
 
 class ResNet_Block(nn.Module):
     def __init__(self, in_chs, out_chs, strides, Nbits=None, symmetric=False):
+        # assert 0 == 1, "don't use this yet"
         super(ResNet_Block, self).__init__()
         self.conv1 = nn.Sequential(
             FP_Conv(in_channels=in_chs, out_channels=out_chs,
@@ -35,8 +36,10 @@ class ResNet_Block(nn.Module):
             x_ = x
         return self.final_activation(x_ + out)
 
+
 class ResNetCIFAR(nn.Module):
     def __init__(self, head_g, num_layers=50, Nbits=None, symmetric=False):
+        # assert 0 == 1, "don't use this yet"
         super(ResNetCIFAR, self).__init__()
         self.num_layers = num_layers
         self.head_conv = nn.Sequential(
@@ -89,10 +92,98 @@ class ResNetCIFAR(nn.Module):
             return self.g_out
 
 
+class ResNet_Block_mine(nn.Module):
+    def __init__(self, in_chs, out_chs, strides):
+        super(ResNet_Block_mine, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=in_chs, out_channels=out_chs,
+                      stride=strides, padding=1, kernel_size=3),
+            nn.BatchNorm2d(out_chs),
+            nn.ReLU(True))
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=out_chs, out_channels=out_chs,
+                      stride=1, padding=1, kernel_size=3),
+            nn.BatchNorm2d(out_chs))
+
+        if in_chs != out_chs:
+            self.id_mapping = nn.Sequential(
+                nn.Conv2d(in_channels=in_chs, out_channels=out_chs,
+                          stride=strides, padding=0, kernel_size=1),
+                nn.BatchNorm2d(out_chs))
+        else:
+            self.id_mapping = None
+        self.final_activation = nn.ReLU(True)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+        if self.id_mapping is not None:
+            x_ = self.id_mapping(x)
+        else:
+            x_ = x
+        return self.final_activation(x_ + out)
+
+
+class ResNetCIFAR_mine(nn.Module):
+    def __init__(self, num_layers):
+        super(ResNetCIFAR_mine, self).__init__()
+        self.num_layers = num_layers
+        self.head_conv = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=16,
+                      stride=1, padding=1, kernel_size=3),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True)
+        )
+        num_layers_per_stage = (num_layers - 2) // 6
+        self.body_op = []
+        num_inputs = 16
+        # Stage 1
+        for j in range(num_layers_per_stage):
+            strides = 1
+            self.body_op.append(ResNet_Block_mine(num_inputs, 16, strides))
+            num_inputs = 16
+        # Stage 2
+        for j in range(num_layers_per_stage):
+            if j == 0:
+                strides = 2
+            else:
+                strides = 1
+            self.body_op.append(ResNet_Block_mine(num_inputs, 32, strides))
+            num_inputs = 32
+        # Stage 2
+        for j in range(num_layers_per_stage):
+            if j == 0:
+                strides = 2
+            else:
+                strides = 1
+            self.body_op.append(ResNet_Block_mine(num_inputs, 64, strides))
+            num_inputs = 64
+            
+        self.body_op = nn.Sequential(*self.body_op)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        # self.head_g = FP_Linear(64, 10, Nbits=None)
+        self.head_g = nn.Sequential(nn.Linear(64, 64), nn.ReLU(True), nn.Linear(64, 64))
+
+    def forward(self, x):
+        out = self.head_conv(x)
+        out = self.body_op(out)
+        self.features = self.avg_pool(out)
+        self.feat_1d = self.features.mean(3).mean(2)  # this is h=f(*)
+        return F.normalize(self.head_g(self.feat_1d), dim=1)
+        # if self.head_g is not None:  # for finetuning and training, 
+        # # where head isn't separate from the rest of the ResNet. See algorithm 1 in SimCLR paper. 
+        #     self.g_out = self.head_g(self.feat_1d)  # this is z=g(h)=g(f(*))
+        #     return self.g_out  # note: g(*) does not reduce # of coordinates to 10, i.e., no logits
+        # else:  # this is for linear evaluation only, where the head is separate
+        #     self.g_out = self.feat_1d
+        #     return self.g_out
+
+
 class LinearEvaluation(nn.Module):
     def __init__(self, method, resnet_model_pth, Nbits=None, symmetric=False):
         # three heads: identity mapping; (2) linear projection; and (3) nonlinear projection
         # nonlinear: W2 * ReLu(W1 * h), where h=ResNet(*)=f(*)
+        assert 0 == 1, "don't use this module yet"
         self.method = method
         self.valid_method = {'identity': None, 
                             'lin': FP_Linear(64, 64, Nbits=None), 
@@ -121,21 +212,37 @@ class LinearEvaluation(nn.Module):
         return self.logits  # note: use cross entropy loss to do logistic regression
 
 
-class ConstantWithWarmup(torch.optim.lr_scheduler._LRScheduler):
-    def __init__(
-        self,
-        optimizer,
-        num_warmup_steps: int,
-    ):
-        self.num_warmup_steps = num_warmup_steps
-        super().__init__(optimizer)
+# https://zablo.net/blog/post/understanding-implementing-simclr-guide-eli5-pytorch/
+# vectorised version
+class ContrastiveLoss(nn.Module):
+    def __init__(self, batch_size, temperature=0.5):
+        super().__init__()
+        self.batch_size = batch_size
+        self.register_buffer("temperature", torch.tensor(temperature))
+        self.register_buffer("negatives_mask", (~torch.eye(batch_size * 2, batch_size * 2, dtype=bool)).float())
+            
+    def forward(self, emb_i, emb_j):
+        """
+        emb_i and emb_j are batches of embeddings, where corresponding indices are pairs
+        z_i, z_j as per SimCLR paper
+        """
+        z_i = F.normalize(emb_i, dim=1)
+        z_j = F.normalize(emb_j, dim=1)
 
-    def get_lr(self):
-        if self._step_count <= self.num_warmup_steps:
-            # warmup
-            scale = 1.0 - (self.num_warmup_steps - self._step_count) / self.num_warmup_steps
-            lr = [base_lr * scale for base_lr in self.base_lrs]
-            self.last_lr = lr
-        else:
-            lr = self.base_lrs
-        return lr
+        representations = torch.cat([z_i, z_j], dim=0)
+        similarity_matrix = F.cosine_similarity(representations.unsqueeze(1), representations.unsqueeze(0), dim=2)
+        
+        sim_ij = torch.diag(similarity_matrix, emb_i.size(0))
+        sim_ji = torch.diag(similarity_matrix, -emb_i.size(0))
+        positives = torch.cat([sim_ij, sim_ji], dim=0)
+        if emb_i.size(0) != self.batch_size:
+            nominator = torch.exp(positives / self.temperature)
+            denominator = (~torch.eye(emb_i.size(0)*2, emb_i.size(0)*2, dtype=bool)).float().to(emb_i.device) * torch.exp(similarity_matrix / self.temperature)
+        else: 
+            nominator = torch.exp(positives / self.temperature)
+            denominator = self.negatives_mask.to(emb_i.device) * torch.exp(similarity_matrix / self.temperature)
+    
+        loss_partial = -torch.log(nominator / torch.sum(denominator, dim=1) + 1e-8)
+        loss = torch.sum(loss_partial) / (2 * emb_i.size(0))
+        # print(torch.norm(emb_i.clone().detach(), p=2), torch.norm(emb_j.clone().detach(), p=2))
+        return loss
--- project_test/train_util.py
+++ project_test/train_util.py
@@ -17,7 +17,7 @@ from lars.lars import LARS
 
 from FP_layers import *
 
-from resnet import ResNetCIFAR
+from resnet import ResNetCIFAR, ContrastiveLoss
 
 # device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -34,7 +34,7 @@ def ddp_setup(rank: int, world_size: int):
     return
 
 
-def nt_xent_loss(x, temperature=0.1):
+def nt_xent_loss(x, temperature=0.5):
     # xcs = F.cosine_similarity(x[None, :, :].to('cpu'), x[:, None, :].to('cpu'), dim=-1)
     xcs = F.cosine_similarity(x[None, :, :], x[:, None, :], dim=-1)
     # assume x has shape [batch_size*2, 64]
@@ -63,8 +63,25 @@ def augment_data(batch):
     # new_batch should be size [batch_size*2, 1, 32, 32]
     new_batch[::2, :, :, :] = augment(batch)
     new_batch[1::2, :, :, :] = augment(batch)
-    del batch
+    # new_batch[::2, :, :, :] = batch
+    # new_batch[1::2, :, :, :] = batch
+    # del batch
     return new_batch
+
+
+def augment_data_separate(batch):
+    """
+    Apply transforms once on one batch without reserving space for the others
+    """
+    augment = transforms.Compose([
+                                  transforms.RandomCrop(32, padding=4),
+                                  transforms.RandomHorizontalFlip(),
+                                  transforms.RandomVerticalFlip(),  # extra vertical flip?
+                                  torchvision.transforms.ColorJitter(0.5)
+                                 ])
+    # augment(batch) should be size [batch_size*1, 1, 32, 32]
+    return augment(batch)
+    # return batch  ## debugging only, no augmentation at all
 
 
 def train(rank, world_size, epochs, batch_size, lr, reg, head, log_every_n=50):
@@ -246,7 +263,7 @@ def train_no_DDP(net, epochs, batch_size, lr, reg, log_every_n=50):
 
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=16)
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_test)
     testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
     # criterion = nn.CrossEntropyLoss()
@@ -276,12 +293,11 @@ def train_no_DDP(net, epochs, batch_size, lr, reg, log_every_n=50):
         # correct = 0
         total = 0
         for batch_idx, (inputs, targets) in enumerate(trainloader):
-            augment_inputs = augment_data(inputs)
-            del inputs
-            augment_inputs, targets = augment_inputs.to(device), targets.to(device)
+            augment_inputs = augment_data(inputs).to(device)
+            # del inputs
             optimizer.zero_grad()
             outputs = net(augment_inputs)
-            del augment_inputs
+            # del augment_inputs
             # loss = criterion(outputs, targets)
             loss = criterion(outputs)
             loss.backward()
@@ -341,6 +357,142 @@ def train_no_DDP(net, epochs, batch_size, lr, reg, log_every_n=50):
                 torch.save(net.state_dict(), "test_model_epoch_%d.pt" % int(epoch))
     return
 
+
+def train_nt_xet_class(net, epochs, batch_size, lr, reg, log_every_n=50):
+    """
+    Training a network
+    :param net: Network for training
+    :param epochs: Number of epochs in total.
+    :param batch_size: Batch size for training.
+    """
+    print('device', device)
+    print('==> Preparing data..')
+    transform_train = transforms.Compose([
+        # transforms.RandomCrop(32, padding=4),
+        # transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),  # transforms happen for each epoch
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    best_loss = 1e5  # best test accuracy
+    start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform_train)
+    # orig_batch_size = batch_size
+    # every_nth_minibatch = int(1)
+    
+    # if orig_batch_size > 2048:  # batch size too large, 
+    #     # see this link https://discuss.pytorch.org/t/how-can-you-train-your-model-on-large-batches-when-your-gpu-can-t-hold-more-than-a-few-samples/80581/4
+    #     batch_size = 2048  # this is max "batch_size" that's actually allowed. orig_batch_size > 2048 is 
+    #     # simulated using 1x update for the optimiser per multiple batches (of size 2048). 
+    #     # every_nth_minibatch = 2048: this is to enable training using large batch size without out-of-memory
+    #     every_nth_minibatch = int(orig_batch_size / batch_size)
+    # assert orig_batch_size % batch_size == 0
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=16)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+
+    # criterion = nn.CrossEntropyLoss()
+    # criterion = ContrastiveLossELI5(batch_size, temperature=0.5, verbose=False)
+    criterion = ContrastiveLoss(batch_size, temperature=0.5)
+
+    # optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.875, weight_decay=reg, nesterov=False)
+    optimizer = LARS(net.parameters(), lr=lr, momentum=0.9, weight_decay=reg, nesterov=False)
+    # scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[int(epochs*0.5), int(epochs*0.75)], gamma=0.1)
+    warmup_iters = 10
+    scheduler_warmup = optim.lr_scheduler.LinearLR(optimizer, start_factor=0.01, end_factor=1.0, total_iters=warmup_iters, verbose=True)
+    scheduler_after  = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs, verbose=True)
+    # T_0 = epochs -> no restart? Paper uses lin warmup (first 10 epochs) then cosine decay schedule w/o restarts
+
+    global_steps = 0
+    start = time.time()
+
+    for epoch in range(start_epoch, epochs):
+        if epoch >= warmup_iters:
+            scheduler = scheduler_after
+        else:
+            scheduler = scheduler_warmup
+        """
+        Start the training code.
+        """
+        print('\nEpoch: %d' % epoch)
+        net.train()
+        train_loss = 0
+        # correct = 0
+        total = 0
+        for batch_idx, (inputs, targets) in enumerate(trainloader):
+            augment_inputs1 = augment_data_separate(inputs).to(device)
+            augment_inputs2 = augment_data_separate(inputs).to(device)
+            # del inputs
+            optimizer.zero_grad()
+            outputs1 = net(augment_inputs1)
+            outputs2 = net(augment_inputs2)
+
+            isnan = sum(torch.isnan(torch.tensor(torch.cat((outputs1.clone().detach(), outputs2.clone().detach())))).to('cpu').numpy().astype(int).flatten())
+            if isnan != 0: 
+                print(isnan)
+            # del augment_inputs1
+            # del augment_inputs2
+            loss = criterion(outputs1, outputs2)
+            loss.backward()
+            optimizer.step()
+            # if batch_size < orig_batch_size:  # if batch size is very very large, 
+            #     # update every batch size.
+            #     if batch_idx % every_nth_minibatch == 0:
+            #         optimizer.step()
+            # else:
+            #     optimizer.step()
+            train_loss += loss.item()
+            # _, predicted = outputs.max(1)
+            total += targets.size(0)
+            # correct += predicted.eq(targets).sum().item()
+            global_steps += 1
+
+            if global_steps % log_every_n == 0:
+                end = time.time()
+                num_examples_per_second = log_every_n * batch_size / (end - start)
+                print("[Step=%d]\tLoss=%.4f\t%.1f examples/second" 
+                        % (global_steps, train_loss / (batch_idx + 1), num_examples_per_second))
+                # print("[Step=%d]\tLoss=%.4f\tacc=%.4f\t%.1f examples/second"
+                    #   % (global_steps, train_loss / (batch_idx + 1), (correct / total), num_examples_per_second))
+                start = time.time()
+
+        scheduler.step()
+
+        """
+        Start the testing code.
+        """
+        net.eval()
+        test_loss = 0
+        # correct = 0
+        total = 0
+        with torch.no_grad():
+            for batch_idx, (inputs, targets) in enumerate(testloader):
+                augment_inputs1, augment_inputs2 = augment_data_separate(inputs).to(device), augment_data_separate(inputs).to(device)
+                outputs1, outputs2 = net(augment_inputs1), net(augment_inputs2)
+                loss = criterion(outputs1, outputs2)
+
+                test_loss += loss.item()
+                # _, predicted = outputs.max(1)
+                total += targets.size(0)
+                # correct += predicted.eq(targets).sum().item()
+        num_val_steps = len(testloader)
+        val_loss = test_loss / num_val_steps
+        # val_acc = correct / total
+        # print("Test Loss=%.4f, Test acc=%.4f" % (test_loss / (num_val_steps), val_acc))
+        print("Test Loss=%.4f" % val_loss)
+
+        if val_loss < best_loss:
+            best_loss = val_loss
+            if epoch % 100 == 0:
+                print("Saving...")
+                torch.save(net.state_dict(), "test_model_epoch_%d.pt" % int(epoch))
+    return
 
 
 def finetune(net, epochs, batch_size, lr, reg, log_every_n=50):
