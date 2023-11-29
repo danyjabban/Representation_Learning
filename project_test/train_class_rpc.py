03260ad129d1bd6f5cb9528ef16e3f749a576f01
Michael Li <zl310@duke.edu>
2023-11-28 23:27:38 -0500

    stopped at scheduler cuz DistributedOptimizer too difficult to support with scheduler

--- project_test/train_class_rpc.py
+++ project_test/train_class_rpc.py
@@ -1,5 +1,6 @@
 import time
 import os
+import threading
 
 import torch
 import torchvision.transforms as transforms
@@ -8,10 +9,9 @@ import torch.nn as nn
 import torch.optim as optim
 
 import torch.multiprocessing as mp
-from torch.utils.data.distributed import DistributedSampler
-from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.distributed import init_process_group, destroy_process_group
-# from torch.trainers import Trainer
+from torch.distributed import rpc
+import torch.distributed.autograd as dist_autograd
+from torch.distributed.optim import DistributedOptimizer
 
 from lars.lars import LARS
 
@@ -58,6 +58,9 @@ class ResNetCIFAR_shard1(nn.Module):
         with self._lock:
             out = self.body_op(x)
         return out.cpu()
+    
+    def parameter_rrefs(self):
+        return [rpc.RRef(p) for p in self.parameters()]
 
 
 
@@ -97,132 +100,46 @@ class ResNetCIFAR_shard2(nn.Module):
             else:  # this is for linear evaluation only, where the head is separate
                 self.g_out = self.feat_1d
         return self.g_out.cpu()
+    
+    def parameter_rrefs(self):
+        return [rpc.RRef(p) for p in self.parameters()]
 
 
-def ddp_setup(rank: int, world_size: int):
-    os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = "12325"  # select any idle port on your machine
 
-    init_process_group(backend="nccl", rank=rank, world_size=world_size)
-    return
-
-
-class TrainerDDP():
-    def __init__(
-        self, rank, model,
-        batch_size, lr, reg, log_every_n=50
-    ) -> None:
-        # super().__init__(rank, model, trainloader, testloader)
-        super().__init__()
-        # https://discuss.pytorch.org/t/extra-10gb-memory-on-gpu-0-in-ddp-tutorial/118113
-        torch.cuda.set_device(rank)  # master gpu takes up extra memory
-        torch.cuda.empty_cache()
-
-        # self.model = DDP(ResNetCIFAR(head_g=head, num_layers=50), device_ids=[rank])
-        self.rank = rank
-        self.model = DDP(torch.nn.SyncBatchNorm.convert_sync_batchnorm(model).to(self.rank), device_ids=[self.rank], gradient_as_bucket_view=True)
-        self.batch_size = batch_size
-        self.lr = lr
-        self.reg = reg
-        self.log_every_n = log_every_n
-
-        self.criterion = ContrastiveLoss(batch_size, temperature=0.5)
-        self.optimizer = LARS(self.model.parameters(), lr=self.lr, momentum=0.9, weight_decay=self.reg, nesterov=False)
-        self.warmup_iters = 10
-        self.scheduler_warmup = optim.lr_scheduler.LinearLR(self.optimizer, start_factor=0.01, end_factor=1.0, 
-                                                            total_iters=self.warmup_iters, verbose=False)
-        self.scheduler_after  = optim.lr_scheduler.CosineAnnealingLR(self.optimizer, T_max=1000, verbose=False)
-        self.trainloader, self.testloader, self.sampler_train = TrainerDDP.cifar_dataloader_ddp(self.batch_size)
-
-        self.scaler = torch.cuda.amp.GradScaler()
-        self.global_steps = 0
+class ResNetCIFAR_Dist(nn.Module):
+    def __init__(self, num_split, workers, head_g, num_layers=50, Nbits=None, symmetric=False):
+        super(ResNetCIFAR_Dist, self).__init__()
+        self.num_split = num_split
+        self.p1_rref = rpc.remote(
+            workers[0],
+            ResNetCIFAR_shard1,
+            args = ("cuda:0", num_layers, Nbits, symmetric)
+        )
+        self.p2_rref = rpc.remote(
+            workers[1],
+            ResNetCIFAR_shard2,
+            args=("cuda:1", head_g, num_layers, Nbits, symmetric)
+        )
         return
 
-    def _run_epoch(self, epoch):
-        self.global_steps = 0
+    def forward(self, xs):
+        out_futures = []
+        for x in iter(xs.split(self.num_split, dim=0)):
+            x_rref = rpc.RRef(x)
+            y_rref = self.p1_rref.remote().forward(x_rref)
+            z_fut = self.p2_rref.rpc_async().forward(y_rref)
+            out_futures.append(z_fut)
 
-        if epoch >= self.warmup_iters:
-            scheduler = self.scheduler_after
-        else:
-            scheduler = self.scheduler_warmup
-        """
-        Start the training code.
-        """
-        if self.rank == 0:
-            print('\nEpoch: %d' % epoch)
-        self.model.train()
-        train_loss = 0
-        total = 0
-        for batch_idx, (inputs, targets) in enumerate(self.trainloader):
-            augment_inputs1 = TrainerDDP.aug_dat(inputs).to(self.rank)
-            augment_inputs2 = TrainerDDP.aug_dat(inputs).to(self.rank)
-            del inputs
-            self.optimizer.zero_grad()
-            with torch.amp.autocast(device_type='cuda', dtype=torch.float16):
-                outputs1 = self.model(augment_inputs1)
-                outputs2 = self.model(augment_inputs2)
-                del augment_inputs1
-                del augment_inputs2
-                loss = self.criterion(outputs1, outputs2)
-            # loss.backward()
-            isnan = sum(torch.isnan(torch.tensor(torch.cat((outputs1.clone().detach(), outputs2.clone().detach())).clone().detach())).to('cpu').numpy().astype(int).flatten())
-            del outputs1, outputs2
-            self.scaler.scale(loss).backward()
-            if isnan != 0: 
-                print(isnan)
-            # self.optimizer.step()
-            self.scaler.step(self.optimizer)
-            self.scaler.update()
-            train_loss += loss.item()
-            total += targets.size(0)
-            self.global_steps += 1
+        return torch.cat(torch.futures.wait_all(out_futures))
+    
+    def parameter_rrefs(self):
+        remote_params = []
+        remote_params.extend(self.p1_rref.remote().parameter_rrefs().to_here())
+        remote_params.extend(self.p2_rref.remote().parameter_rrefs().to_here())
+        return remote_params
 
-            if self.global_steps % self.log_every_n == 0:
-                if self.rank == 0:
-                    print("[Step=%d]\tLoss=%.4f" 
-                            % (self.global_steps, train_loss / (batch_idx + 1)))
 
-            scheduler.step()
-
-        """
-        Start the testing code.
-        """
-        self.model.eval()
-        test_loss = 0
-        # correct = 0
-        total = 0
-        with torch.no_grad():
-            for batch_idx, (inputs, targets) in enumerate(self.testloader):
-                augment_inputs1, augment_inputs2 = TrainerDDP.aug_dat(inputs).to(self.rank), TrainerDDP.aug_dat(inputs).to(self.rank)
-                outputs1, outputs2 = self.model(augment_inputs1), self.model(augment_inputs2)
-                loss = self.criterion(outputs1, outputs2)
-
-                test_loss += loss.item()
-                total += targets.size(0)
-        num_val_steps = len(self.testloader)
-        val_loss = test_loss / num_val_steps
-        if self.rank == 0:
-            print("Test Loss=%.4f" % val_loss)
-        return
-
-    def _save_checkpoint(self, epoch: int, save_base_path: str):
-        print("Saving...")
-        torch.save(self.model.state_dict(), "%s/epoch_%d_bs_%d_lr_%g_reg_%g.pt" 
-                                        % (save_base_path, int(epoch), int(self.batch_size), self.lr, self.reg))
-        return
-
-    def train(self, max_epochs: int, save_base_path: str):
-        for epoch in range(max_epochs):
-            # https://pytorch.org/docs/stable/data.html#torch.utils.data.distributed.DistributedSampler
-            self.sampler_train.set_epoch(epoch)
-            self._run_epoch(epoch)
-            # only save once on master gpu
-            if self.rank == 0 and ((epoch+1) % 100 == 0 or epoch == 0):
-                self._save_checkpoint(epoch, save_base_path)
-        # save last epoch
-        self._save_checkpoint(max_epochs - 1, save_base_path)
-
-    @staticmethod
+def train_rpc(num_split, max_epochs, batch_size, lr, reg, head, save_base_path, log_every_n=50):
     def aug_dat(batch):
         """
         Apply transforms once on one batch without reserving space for the others
@@ -236,8 +153,7 @@ class TrainerDDP():
         # augment(batch) should be size [batch_size*1, 1, 32, 32]
         return augment(batch)
     
-    @staticmethod
-    def cifar_dataloader_ddp(bs: int):
+    def cifar_dataloader_wo_ddp(bs: int):
         transform_gen = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
@@ -245,24 +161,140 @@ class TrainerDDP():
         
         trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform_gen)
 
-        sampler_train = DistributedSampler(trainset)
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=False, sampler=sampler_train, 
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=False,
                                                   num_workers=16, pin_memory=True)
 
         testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_gen)
-        sampler_test = DistributedSampler(testset, shuffle=False)
-        testloader = torch.utils.data.DataLoader(testset, batch_size=bs, shuffle=False, 
-                                                 sampler=sampler_test, num_workers=8, pin_memory=True)
-        return trainloader, testloader, sampler_train
+        testloader = torch.utils.data.DataLoader(testset, batch_size=bs, shuffle=False, num_workers=8, pin_memory=True)
+        return trainloader, testloader
+    
+    def _run_epoch(epoch):
+
+        if epoch >= warmup_iters:
+            scheduler = scheduler_after
+        else:
+            scheduler = scheduler_warmup
+        """
+        Start the training code.
+        """
+        print('\nEpoch: %d' % epoch)
+        model.train()
+        train_loss = 0
+        total = 0
+        for batch_idx, (inputs, targets) in enumerate(trainloader):
+            augment_inputs1 = Trainer_wo_DDP.aug_dat(inputs).to(device)
+            augment_inputs2 = Trainer_wo_DDP.aug_dat(inputs).to(device)
+            del inputs
+            self.optimizer.zero_grad()
+            with dist_autograd.context() as context_id:
+                outputs1 = model(augment_inputs1)
+                outputs2 = model(augment_inputs2)
+                del augment_inputs1
+                del augment_inputs2
+                loss = criterion(outputs1, outputs2)
+                del outputs1, outputs2
+                dist_autograd.backward(context_id, [loss])
+            # self.optimizer.step()
+            optimizer.step(context_id)
+
+            train_loss += loss.item()
+            total += targets.size(0)
+            global_steps += 1
+
+            if global_steps % log_every_n == 0:
+                print("[Step=%d]\tLoss=%.4f" % (global_steps, train_loss / (batch_idx + 1)))
+
+            scheduler.step()
+
+        """
+        Start the testing code.
+        """
+        model.eval()
+        test_loss = 0
+        # correct = 0
+        total = 0
+        with torch.no_grad():
+            for batch_idx, (inputs, targets) in enumerate(testloader):
+                augment_inputs1, augment_inputs2 = Trainer_wo_DDP.aug_dat(inputs).to(device), Trainer_wo_DDP.aug_dat(inputs).to(device)
+                outputs1, outputs2 = model(augment_inputs1), model(augment_inputs2)
+                loss = criterion(outputs1, outputs2)
+
+                test_loss += loss.item()
+                total += targets.size(0)
+        num_val_steps = len(testloader)
+        val_loss = test_loss / num_val_steps
+        print("Test Loss=%.4f" % val_loss)
+        return
+
+    def _save_checkpoint(epoch: int, save_base_path: str):
+        print("Saving...")
+        torch.save(self.model.state_dict(), "%s/epoch_%d_bs_%d_lr_%g_reg_%g.pt" 
+                                        % (save_base_path, int(epoch), int(batch_size), self.lr, self.reg))
+        return
+    # https://discuss.pytorch.org/t/extra-10gb-memory-on-gpu-0-in-ddp-tutorial/118113
+    model = ResNetCIFAR_Dist(num_split, ["master", "worker1"], head_g=head)
+    criterion = ContrastiveLoss(batch_size, temperature=0.5)
+    optimizer = DistributedOptimizer(
+                        LARS, model.parameter_rrefs(), lr=lr, momentum=0.9, weight_decay=reg, nesterov=False
+                        )
+    
+    def create_lr_schheduler(opt_rref):
+        return optim.lr_scheduler.LinearLR(opt_rref, start_factor=0.01, end_factor=1.0, 
+                                                        total_iters=warmup_iters, verbose=False)
+
+    def lrs_step(lrs_rref):
+        lrs_rref.local_value().step()
+
+    lrs_rrefs = []
+    for opt_rref in optimizer.remote_optimizers:
+        lrs_rrefs = rpc.remote(opt_rref.owner(), create_lr_schheduler, args=(opt_rref,))
+
+    with dist_autograd.context() as context_id:
+        # omitting forward-backward-optstep here
+        futs = []
+        for lrs_rref iin lrs_rrefs:
+            futs.append(rpc.rpc_async(lrs_rref.owner(), lrs_step, args=(lrs_rref,)))
+        [fut.wait() for fut in futs]
+    warmup_iters = 10
+    scheduler_warmup = optim.lr_scheduler.LinearLR(optimizer.optimizer, start_factor=0.01, end_factor=1.0, 
+                                                        total_iters=warmup_iters, verbose=False)
+    scheduler_after  = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=1000, verbose=False)
+    trainloader, testloader = Trainer_wo_DDP.cifar_dataloader_wo_ddp(batch_size)
+    global_steps = 0
+
+    for epoch in range(max_epochs):
+        _run_epoch(epoch)
+        # only save once on master gpu
+        if torch.distributed.get_rani() == 0 and ((epoch+1) % 100 == 0 or epoch == 0):
+            _save_checkpoint(epoch, save_base_path)
+    # save last epoch
+    _save_checkpoint(max_epochs - 1, save_base_path)
+    return
 
 
-def run_main(rank, world_size, total_epochs, batch_size, lr, reg, head, save_base_path, log_every_n):
-    ddp_setup(rank, world_size)
-    model = ResNetCIFAR(head_g=head)
-    trainer = TrainerDDP(rank, model, batch_size, lr, reg, log_every_n=log_every_n)
-    # note: rank == rank
-    trainer.train(total_epochs, save_base_path)
-    destroy_process_group()
+def run_worker(rank, world_size, num_split, max_epochs, batch_size, lr, reg, head, save_base_path, log_every_n=50):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '29500'
+    options = rpc.TensorPipeRpcBackendOptions(num_worker_threads=128)
+    if rank == 0:
+        rpc.init_rpc(
+            "master",
+            rank=rank,
+            world_size=world_size,
+            rpc_backend_options=options
+        )
+        train_rpc(num_split, max_epochs, batch_size, lr, reg, head, save_base_path, log_every_n=log_every_n)
+    else:
+        rpc.init_rpc(
+            f"worker{rank}",
+            rank=rank,
+            world_size=world_size,
+            rpc_backend_options=options
+        )
+        pass
+    # block until all rpcs finish
+    rpc.shutdown()
+    return
 
 
 if __name__ == "__main__":
@@ -272,6 +304,12 @@ if __name__ == "__main__":
     os.makedirs(save_base_path, exist_ok=True)
     # model = nn.parallel.DistributedDataParallel(ResNetCIFAR(head_g=head))
     # model = nn.DataParallel(ResNetCIFAR(head_g=head))
-    batch_size = int(2048)
+    batch_size = int(1024)
     world_size = torch.cuda.device_count()
-    mp.spawn(run_main, args=(world_size, 1000, batch_size, 0.3*batch_size/256, 1e-6, head, save_base_path, 50,), nprocs=world_size)
+    num_split = 2
+    # num_split, batch_size, lr, reg, max_epochs, head, save_base_path, log_every_n
+    # tik = time.time() world_size, 1000, batch_size, 0.3*batch_size/256, 1e-6, head, save_base_path, 50,
+    mp.spawn(run_worker, args=(world_size, num_split, 1000, batch_size, 0.3*batch_size/256, 1e-6, head, save_base_path, 50,), 
+                               nprocs=world_size, join=True)
+    # tok = time.time()
+    # print(f"number of splits = {num_split}, execution time = {tok - tik}")
