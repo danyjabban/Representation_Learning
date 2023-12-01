9d97548dccec527c448ddcf158574f5c0aeacad7
Michael Li <zl310@duke.edu>
2023-12-01 16:17:33 -0500

    deleted some commented lines and use Dany's data_util

--- project_test/train_classes.py
+++ project_test/train_classes.py
@@ -16,14 +16,14 @@ from resnet import ResNetCIFAR, ContrastiveLoss
 
 from tqdm import tqdm
 
-from resnet import LinearEvaluation
+# from resnet import LinearEvaluation
 
 from data_utils import *
 
 
 class Trainer_wo_DDP():
     def __init__(
-        self, model, which_device,
+        self, model: ResNetCIFAR, which_device,
         batch_size, lr, reg, train_for_finetune: int, log_every_n=50, write=True
     ) -> None:
         super().__init__()
@@ -41,7 +41,6 @@ class Trainer_wo_DDP():
         self.log_every_n = log_every_n
 
         self.criterion = ContrastiveLoss(batch_size, temperature=0.5)
-        # self.criterion = Trainer_wo_DDP.add_contrastive_loss
         self.optimizer = LARS(self.model.parameters(), lr=self.lr, momentum=0.9, weight_decay=self.reg, nesterov=False)
         self.warmup_iters = 10
         self.scheduler_warmup = optim.lr_scheduler.LinearLR(self.optimizer, start_factor=0.01, end_factor=1.0, 
@@ -51,7 +50,6 @@ class Trainer_wo_DDP():
         self.global_steps = 0
 
         self.write = write
-        # self.scaler = torch.cuda.amp.GradScaler()
         return
 
     def _run_epoch(self, epoch):
@@ -67,39 +65,24 @@ class Trainer_wo_DDP():
         self.model.train()
         train_loss = 0
         total = 0
-        for batch_idx, (inputs, targets) in enumerate(self.trainloader):
-            augment_inputs1 = Trainer_wo_DDP.aug_dat(inputs).to(self.device)
-            augment_inputs2 = Trainer_wo_DDP.aug_dat(inputs).to(self.device)
-            del inputs
+        for batch_idx, (augment_inputs1, augment_inputs2, targets) in enumerate(self.trainloader):
+            # augment_inputs1 = Trainer_wo_DDP.aug_dat(inputs).to(self.device)
+            # augment_inputs2 = Trainer_wo_DDP.aug_dat(inputs).to(self.device)
             
-            # with torch.amp.autocast(device_type='cuda', dtype=torch.float16):
-            outputs1 = self.model(augment_inputs1)
-            outputs2 = self.model(augment_inputs2)
-            del augment_inputs1
-            del augment_inputs2
+            outputs1, outputs2 = self.model(augment_inputs1.to(self.device)), self.model(augment_inputs2.to(self.device))
             loss = self.criterion(outputs1, outputs2)
-            # loss = self.criterion(torch.concat([outputs1, outputs2], dim=1))
             loss.backward()
-            isnan = sum(torch.isnan(torch.tensor(torch.cat((outputs1.clone().detach(), outputs2.clone().detach())).clone().detach())).to('cpu').numpy().astype(int).flatten())
-            del outputs1, outputs2
-            # self.scaler.scale(loss).backward()
-            if isnan != 0: 
-                print(isnan)
-            # self.optimizer.step()
             if (batch_idx+1) % self.acc_steps == 0 or (batch_idx+1 == len(self.trainloader)):
                 self.optimizer.step()
                 self.optimizer.zero_grad()
                 scheduler.step()
-                # self.scaler.step(self.optimizer)
-            # self.scaler.update()
 
             train_loss += loss.item()
             total += targets.size(0)
             self.global_steps += 1
 
             if self.global_steps % (self.log_every_n * self.acc_steps) == 0:
-                print("[Step=%d]\tLoss=%.4f" 
-                        % (self.global_steps, train_loss / (batch_idx + 1)))
+                print("[Step=%d]\tLoss=%.4f" % (self.global_steps, train_loss / (batch_idx + 1)))
         # end training for this epoch
 
         """
@@ -110,9 +93,9 @@ class Trainer_wo_DDP():
         # correct = 0
         total = 0
         with torch.no_grad():
-            for batch_idx, (inputs, targets) in enumerate(self.testloader):
-                augment_inputs1, augment_inputs2 = Trainer_wo_DDP.aug_dat(inputs).to(self.device), Trainer_wo_DDP.aug_dat(inputs).to(self.device)
-                outputs1, outputs2 = self.model(augment_inputs1), self.model(augment_inputs2)
+            for batch_idx, (augment_inputs1, augment_inputs2, targets) in enumerate(self.testloader):
+                # augment_inputs1, augment_inputs2 = Trainer_wo_DDP.aug_dat(inputs).to(self.device), Trainer_wo_DDP.aug_dat(inputs).to(self.device)
+                outputs1, outputs2 = self.model(augment_inputs1.to(self.device)), self.model(augment_inputs2.to(self.device))
                 loss = self.criterion(outputs1, outputs2)
 
                 test_loss += loss.item()
@@ -124,60 +107,57 @@ class Trainer_wo_DDP():
 
     def _save_checkpoint(self, epoch: int, save_base_path: str):
         print("Saving...")
-        torch.save(self.model.state_dict(), "%s/epoch_%d_bs_%d_lr_%g_reg_%g.pt" 
+        torch.save(self.model.state_dict(), "%s/epoch_%d_bs_%d_lr_%g_reg_%g_embedDim_%d.pt" 
                                         % (save_base_path, int(epoch), int(self.batch_size * self.acc_steps),
-                                           self.lr, self.reg))
+                                           self.lr, self.reg, self.model.embed_dim))
         return
 
     def train(self, max_epochs: int, save_base_path: str):
-        file_name = "%s/max_epoch_%d_bs_%d_lr_%g_reg_%g.txt" % (save_base_path, int(max_epochs), int(self.batch_size * self.acc_steps), self.lr, self.reg)
+        file_name = "%s/max_epoch_%d_bs_%d_lr_%g_reg_%g_embedDim_%d.txt" \
+            % (save_base_path, int(max_epochs), int(self.batch_size * self.acc_steps), self.lr, self.reg, self.model.embed_dim)
         if file_name not in os.listdir(save_base_path) and self.write:
             f_ptr = open(file_name, 'w')
             f_ptr.close()
-        self.optimizer.zero_grad()  # just in case, since I moved self.optimizer.zero_grad() to bottom of 1x iteration 
-        # in _run_epoch
+        self.optimizer.zero_grad()  # just in case, since I moved self.optimizer.zero_grad() to bottom of 1x iteration in _run_epoch
         for epoch in tqdm(range(max_epochs), desc='ResNet_bs_%d' % (self.batch_size * self.acc_steps)):
-            
             val_loss = self._run_epoch(epoch)  # whether optimizer step happens is determined by batch_idx, not epoch
             if self.write:
                 f_ptr = open(file_name, 'a')
                 f_ptr.write("%d,%.6f\n" % (epoch, val_loss))
                 f_ptr.close()
-            # only save once on master gpu
             if (epoch+1) % 100 == 0:  # note: since counting from 0 -> when saving add 1.
                 self._save_checkpoint(epoch+1, save_base_path)
-        # # save last epoch
-        # self._save_checkpoint(max_epochs - 1, save_base_path)
 
-    @staticmethod
-    def aug_dat(batch):
-        """
-        Apply transforms once on one batch without reserving space for the others
-        """
-        augment = transforms.Compose([
-                                    transforms.RandomCrop(32, padding=4),
-                                    transforms.RandomHorizontalFlip(p=0.5),
-                                    # transforms.RandomVerticalFlip(p=0.5),  # extra vertical flip?
-                                    torchvision.transforms.ColorJitter(0.5)
-                                    ])
-        # augment(batch) should be size [batch_size*1, 1, 32, 32]
-        return augment(batch)
+    # @staticmethod
+    # def aug_dat(batch):
+    #     """
+    #     Apply transforms once on one batch without reserving space for the others
+    #     """
+    #     augment = transforms.Compose([
+    #                                 transforms.RandomCrop(32, padding=4),
+    #                                 transforms.RandomHorizontalFlip(p=0.5),
+    #                                 # transforms.RandomVerticalFlip(p=0.5),  # extra vertical flip?
+    #                                 torchvision.transforms.ColorJitter(0.5)
+    #                                 ])
+    #     # augment(batch) should be size [batch_size*1, 1, 32, 32]
+    #     return augment(batch)
     
     @staticmethod
     def cifar_dataloader_wo_ddp(bs: int, train_for_finetune: int):
-        transform_gen = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ])
-        trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform_gen)
-        # trainset = CIFAR10_SimCLR(root='./data', train=True, download=False, transform=transform_gen)
+        # transform_gen = transforms.Compose([
+        #     transforms.ToTensor(),
+        #     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        # ])
+        # trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform_gen)
+        trainset = CIFAR10_SimCLR(root='./data', train=True)
         if train_for_finetune == 1:
             print("training for eventual fine-tuning")
             trainset, _ = torch.utils.data.random_split(trainset, [0.9, 0.1], generator=torch.Generator().manual_seed(42))
 
         trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=True, num_workers=16, pin_memory=True)
 
-        testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_gen)
+        # testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_gen)
+        testset = CIFAR10_SimCLR(root='./data', train=False)
         testloader = torch.utils.data.DataLoader(testset, batch_size=bs, shuffle=False, num_workers=8, pin_memory=True)
         # print('len(trainloader)', len(trainloader))
         return trainloader, testloader
