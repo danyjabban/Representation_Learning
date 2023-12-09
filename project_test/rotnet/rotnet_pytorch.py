f3b3b36fd5719dd9d5c61b691b3d708d6131ae30
mgray07 <mccullough.gray@gmail.com>
2023-12-09 02:46:02 -0500

    architecture for eval and model

--- project_test/rotnet/rotnet_pytorch.py
+++ project_test/rotnet/rotnet_pytorch.py
@@ -9,18 +9,29 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         padding = (kernel_size-1)/2
         # make sure to make this work with FP layers later!
-        self.feat = nn.Sequential(nn.Conv2d(in_channel, \
-            out_channel, kernel_size=kernel_size, stride=1, \
-                padding=padding, bias=False),
+        self.feat = nn.Sequential(nn.Conv2d(in_channel, 
+                                            out_channel, 
+                                            kernel_size=kernel_size, 
+                                            stride=1,
+                                            padding=padding,
+                                            bias=False),
                                     nn.BatchNorm2d(out_channel),
                                     nn.ReLU(inplace=True))
     
     def forward(self, x):
         out = self.feat(x)
         return out
+    
+class GlobalAveragePooling(nn.Module):
+    def __init__(self):
+        super(GlobalAveragePooling, self).__init__()
+
+    def forward(self, feat):
+        num_channels = feat.size(1)
+        return F.avg_pool2d(feat, (feat.size(2), feat.size(3))).view(-1, num_channels)
 
 class NetworkInNetwork(nn.Module):
-    def __init__(self, num_classes=4, num_stages=3, Nbits=None, symmetric=False, lin_eval_flag=False):
+    def __init__(self, num_classes=4, Nbits=None, symmetric=False, lin_eval_flag=False):
         super(NetworkInNetwork, self).__init__()
         nChannels = 192
         nChannels2 = 160
@@ -40,6 +51,36 @@ class NetworkInNetwork(nn.Module):
                                     BasicBlock(nChannels, nChannels, 1),
                                     BasicBlock(nChannels, nChannels, 1))
         
+        self.block4 = nn.Sequential(BasicBlock(nChannels, nChannels, 3),
+                                    BasicBlock(nChannels, nChannels, 1),
+                                    BasicBlock(nChannels, nChannels, 1))
+        
+        self.globpool = GlobalAveragePooling()
+        
+        self.linear = nn.Linear(nChannels, num_classes)
+        
+        self.init_weights()
     
-    # need to figure out the next step which is the different amount of blocks
-    # also how to make it go down to just 4 classes
+    def forward(self, x):
+        x_block1 = self.block1(x)
+        x_block2 = self.block2(x_block1)
+        x_block3 = self.block3(x_block2)
+        x_block4 = self.block4(x_block3)
+        x_globpool = self.globpool(x_block4)
+        x_linear = self.linear(x_globpool)
+        return x_linear
+    
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                if m.weight.requires_grad:
+                    n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                    m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                if m.weight.requires_grad:
+                    m.weight.data.fill_(1)
+                if m.bias.requires_grad:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                if m.bias.requires_grad:
+                    m.bias.data.zero_()
