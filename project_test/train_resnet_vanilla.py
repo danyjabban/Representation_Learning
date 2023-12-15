f95a1c699f0afc3a2b6db1ab2f84026423ed2cc8
Michael Li <zl310@duke.edu>
2023-12-14 20:11:18 -0500

    done with vanilla resnet training

--- project_test/train_resnet_vanilla.py
+++ project_test/train_resnet_vanilla.py
@@ -1,11 +1,17 @@
 from train_classes import Trainer_Vanilla
 from resnet_pytorch import ResNet_PyTorch_vanilla_wrapper
+
 import torch
+
+import argparse
 import os
 
 
 if __name__ == '__main__':
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--device', type=int, required=True)
+    args = parser.parse_args()
+    device = torch.device('cuda:%d' % args.device if torch.cuda.is_available() else 'cpu')
     save_base_path = "./saved_models/vanilla_resnet/"
     os.makedirs(save_base_path, exist_ok=True)
     
@@ -19,4 +25,4 @@ if __name__ == '__main__':
                               train_bs=train_bs, val_bs=val_bs, 
                               lr=lr, reg=1e-4, save_base_path=save_base_path, 
                               data_path=data_path)
-    trainer.train(max_epochs=1)
+    trainer.train(max_epochs=200)
